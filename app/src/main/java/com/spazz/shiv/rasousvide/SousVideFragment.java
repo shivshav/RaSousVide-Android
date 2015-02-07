523301de2925f3471f8c5ce2f3186644f639aa5c
Shivneil <sprasad0603@gmail.com>
2015-02-07 13:26:35 -0800

    Compiling and blank fragments are appearing now. Ready to start implementing different functionalities within different fraagments

---
+++ app/src/main/java/com/spazz/shiv/rasousvide/SousVideFragment.java
@@ -0,0 +1,114 @@
+package com.spazz.shiv.rasousvide;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link SousVideFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link SousVideFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SousVideFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_POSITION = "position";
+
+    @InjectView(R.id.textView)
+    TextView textView;
+    // TODO: Rename and change types of parameters
+    private int position;
+
+//    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param position Positioning on tab.
+     * @return A new instance of fragment SousVideFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SousVideFragment newInstance(int position) {
+        SousVideFragment fragment = new SousVideFragment();
+        Bundle b = new Bundle();
+        b.putInt(ARG_POSITION, position);
+        fragment.setArguments(b);
+        return fragment;
+    }
+
+    public SousVideFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            position = getArguments().getInt(ARG_POSITION);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_sous_vide, container, false);
+        ButterKnife.inject(this, rootView);
+        ViewCompat.setElevation(rootView, 50);
+        textView.setText("Placeholder son!");
+        return rootView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+//        try {
+//            mListener = (OnFragmentInteractionListener) activity;
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(activity.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+//        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        public void onFragmentInteraction(Uri uri);
+//    }
+
+}
