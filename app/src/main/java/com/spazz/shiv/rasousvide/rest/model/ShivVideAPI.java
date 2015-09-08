b82eb9f0ba1c863bd2741cf94ad3e54bfd504c1c
shiv <sprasad0603@gmail.com>
2015-09-08 12:54:37 -0700

    Changed API for retrofit to take shivvide post as form data and with inputs instead of object

--- app/src/main/java/com/spazz/shiv/rasousvide/rest/model/ShivVideAPI.java
+++ app/src/main/java/com/spazz/shiv/rasousvide/rest/model/ShivVideAPI.java
@@ -1,7 +1,8 @@
 package com.spazz.shiv.rasousvide.rest.model;
 
 
-import retrofit.Callback;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
 import retrofit.http.GET;
 import retrofit.http.POST;
 import rx.Observable;
@@ -13,7 +14,17 @@ public interface ShivVideAPI {
     @GET("/getstatus")
     Observable<ShivVideResponse> getCurrentPiParams();
 
+    @FormUrlEncoded
     @POST("postparams/1")
     //TODO: Add object with data to send to Pi
-    void postDeliciousParams(ShivVidePost post);
+    void postDeliciousParams(
+            @Field("mode") String mode,
+            @Field("setpoint") float setpoint,
+            @Field("k") float kParameter,
+            @Field("i") float iParameter,
+            @Field("d") float dParameter,
+            @Field("dutycycle") float dutyCycle,
+            @Field("cycletime") float cycleTime,
+            @Field("boilManageTemp") float boilManageTemp,
+            @Field("numPntsSmooth") int numSmoothPoint);
 }
